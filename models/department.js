abe2c33119f7b6cdb5a81283a5939f2e059cbb39
skumari04 <swati.kumari@ingrammicro.com>
2020-08-25 18:13:36 +0530

    commiting changes for reference

--- models/department.js
+++ models/department.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const deptSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    // using ref to reference model "employee"
     Employee: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', required: true },
     assigned: { type: Boolean, default: false }
 });
